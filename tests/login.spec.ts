d669d18eadd472981e9b434ac7818b5dc60e4c49
GTyson261 <gatyson261@gmail.com>
2026-06-04 11:18:42 -0400

    exdit playwright to open chrome

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,13 +1,35 @@
 import { test, expect } from '@playwright/test';
 
-test('User can log in successfully', async ({ page }) => {
-  await page.goto('https://practicetestautomation.com/practice-test-login/');
+test('Login Demo', async ({ page }) => {
 
-  await page.fill('#username', 'student');
-  await page.fill('#password', 'Password123');
+console.log('Opening login page...');
+await page.goto(
+'https://practicetestautomation.com/practice-test-login/'
+);
 
-  await page.click('#submit');
+await page.waitForTimeout(1000);
 
-  await expect(page.locator('h1'))
-    .toContainText('Logged In Successfully');
+console.log('Typing username...');
+await page.fill('#username', 'student');
+
+await page.waitForTimeout(1000);
+
+console.log('Typing password...');
+await page.fill('#password', 'Password123');
+
+await page.waitForTimeout(1000);
+
+console.log('Clicking login button...');
+await page.click('#submit');
+
+await page.waitForTimeout(1000);
+
+console.log('Checking login success...');
+await expect(page.locator('h1'))
+.toContainText('Logged In Successfully');
+
+console.log('Test Passed!');
+
+await page.waitForTimeout(2000);
+
 });
